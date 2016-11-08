dbfb39e5baee4e91df50e35f5285c634b62bae2d
erimo832 <erimo832@gmail.com>
2016-11-08 20:40:15 +0100

    Add: temperature page (sweden)

--- Slideviewer/Scripts/common.js
+++ Slideviewer/Scripts/common.js
@@ -11,6 +11,9 @@ function getQueryParam(paramName, defaultValue) {
         query = query.split("%20").join(" ");
         query = query.split("%23").join("#");
         query = query.split("%27").join("'");
+        query = query.split("%C3%B6").join("ö");
+        query = query.split("%C3%A5").join("å");
+        query = query.split("%C3%A4").join("ä");
 
 
         var params = query.split("&");
